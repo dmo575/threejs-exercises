bdb964a40dd6664cc7088b76ff7dea550f4cb1e9
dmo575 <dmo5@protonmail.com>
2023-11-09 19:47:47 -0500

    Updated 02

--- 02/README.md
+++ 02/README.md
@@ -1,2 +1,9 @@
 # About
-Shows how to have a simple "game loop", update function.
+In this iteration the project shows how to have a simple "game loop", update function.
+
+We also add pre-built controls for the camera:
+```
+LMB - rotate around
+RMB - Pan around
+Scroll wheel - scale up/down
+```
--- 02/main.js
+++ 02/main.js
@@ -1,5 +1,5 @@
 import * as THREE from "three"
-import { aspectRatio } from "./aspect_ratio";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
 // demo scene set up. A scene with a perspective camera and a blue cube
 const canvas = document.querySelector("canvas.webgl");
@@ -8,10 +8,14 @@ const mesh = new THREE.Mesh(
   new THREE.BoxGeometry(),
   new THREE.MeshBasicMaterial({color: "blue"})
 );
-const camera = new THREE.PerspectiveCamera(45, aspectRatio.get(), 1, 1000);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 
 const renderer = new THREE.WebGLRenderer({canvas: canvas});
-renderer.setSize(aspectRatio.width, aspectRatio.height);
+// set the pixel ratio of the renderer. This will make the renderer render images with the correct amount of pixels based on our actual physical amount of pixels, unless the
+// pixel ratio is more than 2, at which case it would be too many pixels and we could have performance issues.
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setSize(window.innerWidth, window.innerHeight);
+const controls = new OrbitControls(camera, canvas);
 
 scene.add(mesh);
 scene.add(camera);
@@ -31,7 +35,7 @@ function update() {
   const delta = (currTick - lastTick) / 1000.0;
 
   // update cube's rotation
-  mesh.rotation.y += 1.0 * delta;
+  //mesh.rotation.y += 1.0 * delta;
 
   // render
   renderer.render(scene, camera);
@@ -40,5 +44,23 @@ function update() {
   window.requestAnimationFrame(update);
   lastTick = currTick;
 }
+
+// handle resize
+window.addEventListener("resize", () => {
+  // here we update the camera aspect ratio
+  camera.aspect = window.innerWidth / window.innerHeight;
+
+  // for some reason we need to update the projection matrix too
+  camera.updateProjectionMatrix();
+
+  // we also update the renderer size, which is the canvas. If we dont we would have wrapping in our image because of differences in size between the camera and the canvas,
+  // which would make it so that the image we get from the camera would stretch in and out to fit the canvas.
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // set the pixel ratio of the renderer. This will make the renderer render images with the correct amount of pixels based on our actual physical amount of pixels, unless the
+  // pixel ratio is more than 2, at which case it would be too many pixels and we could have performance issues.
+  // placed onside the resize because someone could drag the browser onto a second monitor.
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 update();
